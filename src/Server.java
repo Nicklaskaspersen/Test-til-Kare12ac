6eabbb50348fb5ed1d893f7a2e1e1daf54df413e
Nicklaskaspersen <Nicklaskaspersen@gmail.com>
2014-09-29 13:25:14 +0200

    Første forsøg - Nicklas

---
+++ src/Server.java
@@ -0,0 +1,9 @@
+
+public class Server {
+
+	/*
+	 * Hej, her kan du fors¿ge at lave lidt. Jeg har delt det hele med dig.
+	 * Husk at clone repository til desktop efterfulgt af at importere det til Eclipse.
+	 */
+	
+}
